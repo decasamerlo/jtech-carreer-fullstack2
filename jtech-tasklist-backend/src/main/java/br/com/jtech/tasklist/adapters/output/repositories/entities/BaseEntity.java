1fd69c91aaf42d08af6f2ba936064cb8dc458bbd
Andre <casagrandamerlo@gmail.com>
2026-07-04 20:03:46 -0300

    feat(audit): add BaseEntity<T> mapped superclass
    JPA base entity with:
    - @Id UUID generation
    - @CreatedDate / @CreatedBy
    - @LastModifiedDate / @LastModifiedBy
    - Soft-delete: deletedAt / deletedBy
    - @Version optimistic locking

---
+++ jtech-tasklist-backend/src/main/java/br/com/jtech/tasklist/adapters/output/repositories/entities/BaseEntity.java
@@ -0,0 +1,74 @@
+package br.com.jtech.tasklist.adapters.output.repositories.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity<T> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private T id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private UUID createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private UUID updatedBy;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    private UUID deletedBy;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    private Integer version;
+
+    public boolean isDeleted() {
+        return deletedAt != null;
+    }
+
+    public void markAsDeleted() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public void markAsDeleted(UUID userId) {
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = userId;
+    }
+
+    public void restore() {
+        this.deletedAt = null;
+        this.deletedBy = null;
+    }
+}
